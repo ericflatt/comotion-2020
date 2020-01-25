14e0609aa9bea6b0ad9b11e0c65f318fe9d1a8c5
Eric Flatt <eflatt18@gmail.com>
2020-01-25 16:18:47 -0500

    speed

--- source/js/app.js
+++ source/js/app.js
@@ -32,7 +32,7 @@ $(document).ready(function(){
 
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 800, function(){
+      }, 1200, function(){
 
 
         window.location.hash = hash;
